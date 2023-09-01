a23eec9afda259058a641ef305578753f39b063c
Santiago Ocampo <sanntiocampo@gmail.com>
2023-09-01 09:09:59 -0300

    Db and TypeScript initial config

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,43 +1,44 @@
-const { Sequelize } = require("sequelize");
-const fs = require("fs");
-const path = require("path");
-require("dotenv").config();
+import { Sequelize } from "sequelize";
+import fs from "fs";
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
   {
-    logging: false, // not set to console.log to see the raw SQL queries
+    logging: false,
   }
 );
 
 const basename = path.basename(__filename);
-const modelDefiners = [];
+const modelDefiners: any[] = [];
 
 fs.readdirSync(path.join(__dirname, "/models"))
   .filter(
-    (file) =>
+    (file: string) =>
       file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
   )
-  .forEach((file) => {
+  .forEach((file: string) => {
     modelDefiners.push(require(path.join(__dirname, "/models", file)));
   });
 
-modelDefiners.forEach((model) => model(sequelize));
+modelDefiners.forEach((model: any) => model(sequelize));
+const modifiedModels: { [key: string]: any } = {};
 let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [
-  entry[0][0].toUpperCase() + entry[0].slice(1),
-  entry[1],
-]);
-sequelize.models = Object.fromEntries(capsEntries);
+entries.forEach((entry: any[]) => {
+  const key = entry[0][0].toUpperCase() + entry[0].slice(1);
+  modifiedModels[key] = entry[1];
+});
 
-const { Videogame, Genre } = sequelize.models;
+const { Videogame, Genre } = modifiedModels;
 
 // Relations
 Videogame.belongsToMany(Genre, { through: "VideogameGenre" });
 Genre.belongsToMany(Videogame, { through: "VideogameGenre" });
 
-module.exports = {
-  ...sequelize.models,
-  conn: sequelize,
-};
+export const models = modifiedModels;
+export const conn = sequelize;
