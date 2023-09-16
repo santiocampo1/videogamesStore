f84259e9e6618a8061a8aefd168d5b6952b000cd
Santiago Ocampo <sanntiocampo@gmail.com>
2023-09-16 11:46:22 -0300

    Landing

--- client/videogames_app/src/Views/Landing/Landing.tsx
+++ client/videogames_app/src/Views/Landing/Landing.tsx
@@ -1,5 +1,20 @@
+import styles from "./Landing.module.css";
+import { NavLink } from "react-router-dom";
+
 const Landing: React.FC = () => {
-  return <div>Landing</div>;
+  return (
+    <div className={styles.container}>
+      <div>
+        <h1>Welcome</h1>
+      </div>
+
+      <div>
+        <NavLink className={styles.navButton} to="/home">
+          Let's Start
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
