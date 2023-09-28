5fbc44a816745ada85b079249e91e3ed3c34353d
Santiago Ocampo <sanntiocampo@gmail.com>
2023-09-28 10:58:31 -0300

    Post videogame

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,22 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import mainRouter from "./routes/mainRouter";
 
 const app = express();
 
 // Middlewares
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204,
+  })
+);
+
 app.use(morgan("dev"));
-app.use(express.json()); // Parser function
+app.use(express.json());
 app.use(mainRouter);
 
 export default app;
