e4f402be0eef7a82bc1317bed7e64059cc39f5cf
Santiago Ocampo <sanntiocampo@gmail.com>
2023-10-11 08:46:35 -0300

    Pagination

--- client/videogames_app/src/Views/Home/Home.tsx
+++ client/videogames_app/src/Views/Home/Home.tsx
@@ -1,6 +1,6 @@
 import styles from "./Home.module.css";
 import Cards from "../../Components/Cards/Cards";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { AnyAction } from "redux";
 import { useDispatch, useSelector } from "react-redux";
 import { ThunkDispatch } from "redux-thunk";
@@ -10,9 +10,25 @@ import { RootState } from "../../Redux/Reducer";
 const Home: React.FC = () => {
   const dispatch =
     useDispatch<ThunkDispatch<RootState, undefined, AnyAction>>();
+  const ITEMS_PER_PAGE = 12;
+
   const allVideogames = useSelector((state: RootState) => state.allVideogames);
   const videogamesOrdered = useSelector(
     (state: RootState) => state.videogamesOrdered
+  );
+
+  const videogamesToShow = videogamesOrdered.length
+    ? videogamesOrdered
+    : allVideogames;
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = Math.ceil(videogamesToShow.length / ITEMS_PER_PAGE);
+
+  const indexOfLastVideogame = currentPage * ITEMS_PER_PAGE;
+  const indexOfFirstVideogame = indexOfLastVideogame - ITEMS_PER_PAGE;
+  const currentVideogames = videogamesToShow.slice(
+    indexOfFirstVideogame,
+    indexOfLastVideogame
   );
 
   useEffect(() => {
@@ -21,6 +37,7 @@ const Home: React.FC = () => {
 
   const onChangeOrder = (event: any) => {
     dispatch(filterByNameVideogames(event.target.value));
+    setCurrentPage(1); // Reset the page when order changes
   };
 
   return (
@@ -28,15 +45,40 @@ const Home: React.FC = () => {
       <h1>Videogames Store</h1>
       <label>Order by Name</label>
       <select onChange={onChangeOrder} name="" id="">
-        <option value="default">-</option>
+        <option defaultChecked value="default">
+          -
+        </option>
         <option value="asc">A-Z</option>
         <option value="des">Z-A</option>
       </select>
-      <Cards
-        allVideogames={
-          videogamesOrdered.length ? videogamesOrdered : allVideogames
-        }
-      />
+
+      <Cards allVideogames={currentVideogames} />
+
+      <div className={styles.paginationControls}>
+        <button onClick={() => setCurrentPage(1)} disabled={currentPage === 1}>
+          First
+        </button>
+        <button
+          onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        <button
+          onClick={() =>
+            setCurrentPage((prev) => Math.min(prev + 1, totalPages))
+          }
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
+        <button
+          onClick={() => setCurrentPage(totalPages)}
+          disabled={currentPage === totalPages}
+        >
+          Last
+        </button>
+      </div>
     </div>
   );
 };
