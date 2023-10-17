9d014c0ff2680b3a4afbedad7dc629128cfc1cae
Santiago Ocampo <sanntiocampo@gmail.com>
2023-10-17 12:24:12 -0300

    searchbar

--- client/videogames_app/src/Components/NavBar/NavBar.tsx
+++ client/videogames_app/src/Components/NavBar/NavBar.tsx
@@ -1,9 +1,16 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 import styles from "./NavBar.module.css";
 import logoNavBar from "../../assets/logoNavBar.png";
 import LogoutButton from "../LogoutButton/LogoutButton";
 
 const NavBar: React.FC = () => {
+  const [searchValue, setSearchValue] = useState<string>("");
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
+
   return (
     <div className={styles.navContainer}>
       <div>
@@ -12,10 +19,20 @@ const NavBar: React.FC = () => {
         </NavLink>
       </div>
 
+      <div className={styles.searchContainer}>
+        <input
+          type="text"
+          value={searchValue}
+          onChange={handleSearchChange}
+          placeholder="Buscar videojuegos..."
+          className={styles.searchInput}
+        />
+      </div>
+
       <div>
         <NavLink to={"/home"}>Home</NavLink>
         <NavLink to={"/create"}>Create</NavLink>
-        <LogoutButton></LogoutButton>
+        <LogoutButton />
       </div>
     </div>
   );
